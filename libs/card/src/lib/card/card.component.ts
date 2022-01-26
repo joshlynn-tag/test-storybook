0e9ffcd722dc60bc6d805a31ef2f6ed9dd45885f
Josh Lynn <josh.lynn@telusagriculture.com>
2022-01-25 16:48:21 -0800

    Initial

---
+++ libs/card/src/lib/card/card.component.ts
@@ -0,0 +1,13 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'test-storybook-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css'],
+  encapsulation:  ViewEncapsulation.None
+})
+export class CardComponent  {
+
+
+
+}
